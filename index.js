40a47feb756e9fe7f29940897daf4b61e50573e4
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-10 21:36:35 +1300

    cubes

---
+++ index.js
@@ -0,0 +1,16 @@
+var a = Math.pow(4 , 3); //creates a variable so that we dont have to type the equation out in future
+var b = Math.pow(8, 3); 
+var c = Math.pow(2 , 3); 
+var d = Math.pow(6 , 3); 
+
+sum = (a + b + c + d); //creates a variable called sum, so that we can use that instead of typing out a,b,c,d, each time
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+console.log(sum); //uses the variable sum rather than having to type out the whole equation each time
+
+
+
